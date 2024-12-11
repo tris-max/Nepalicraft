60a2e550c12e0581a3145ccfd3830925a19a1aef
tris-max <acharya.t@northeastern.edu>
2024-12-11 15:30:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js for the pop up message
+function showConfirm() {
+    var result = confirm("Purchase Now?");
+    if (result) {
+      alert("Continue to the shopping site.");
+    } else {
+      alert("Thank you.");
+    }
+  }
+  
+
